a4c4ad3fec4aec07eb098e5465d132bfed343b7b
Frankk <150551503+Franckou@users.noreply.github.com>
2026-05-14 17:09:29 -0300

    Refactor trigger-noti to handle POST requests
    Updated the trigger-noti function to handle only POST requests and modified the message sent via Pusher.

--- netlify/functions/trigger-noti.js
+++ netlify/functions/trigger-noti.js
@@ -1,18 +1,23 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-    appId: "2154914",
-    key: "2b186dcf533c78bb306b",
-    secret: "feb491a85b5889688fc3",
-    cluster: "sa1",
-    useTLS: true
+  appId: "2154914",
+  key: "2b186dcf533c78bb306b",
+  secret: "feb491a85b5889688fc3",
+  cluster: "sa1",
+  useTLS: true
 });
 
 exports.handler = async function (event, context) {
-    await pusher.trigger("canal-rodaje", "aparecer-alerta", { message: "start" });
+  // Solo permitimos peticiones POST (desde tu botón)
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Metodo no permitido" };
+  }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ status: "Enviado" }),
-    };
+  await pusher.trigger("canal-rodaje", "aparecer-alerta", { message: "exploto" });
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ status: "Señal enviada al set!" }),
+  };
 };
